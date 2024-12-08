9e4901982480174d4ed840d4ab3844a823281138
Kojoel <nanaabruquah1s@gmail.com>
2024-12-08 16:31:02 +0000

    feat: remove grades list and create assessment overview

---
+++ src/app/features/grade-management/assessment-overview/assessment-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assessment-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './assessment-overview.component.html',
+  styleUrl: './assessment-overview.component.scss'
+})
+export class AssessmentOverviewComponent {
+
+}
