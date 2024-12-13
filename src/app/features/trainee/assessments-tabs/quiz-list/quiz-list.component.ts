68b60e7e418117b79ccaaa62af6a096bc135de5d
Kojoel <nanaabruquah1s@gmail.com>
2024-12-13 17:51:25 +0000

    feat: populate template with data from backend

--- src/app/features/trainee/assessments-tabs/quiz-list/quiz-list.component.ts
+++ src/app/features/trainee/assessments-tabs/quiz-list/quiz-list.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf, TitleCasePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { DecodedToken } from '@core/models/iuser';
@@ -12,7 +12,7 @@ import { BehaviorSubject, Observable, combineLatest, filter, map, of, tap } from
 @Component({
   selector: 'app-quiz-list',
   standalone: true,
-  imports: [NgFor, NgIf, AsyncPipe],
+  imports: [NgFor, NgIf, AsyncPipe, TitleCasePipe],
   templateUrl: './quiz-list.component.html',
   styleUrl: './quiz-list.component.scss'
 })
@@ -27,7 +27,8 @@ export class QuizListComponent {
 
 
   assessments$!: Observable<Assignment[]>; // holds the filtered data
-  filteredAssessment$!: Observable<Assignment[]>; // holds the filtered data
+  // filteredAssessment$!: Observable<Assignment[]>; // holds the filtered data
+  filteredAssessment$!: Observable<any[]>; // holds the filtered data
 
   showmore: boolean = true;
 
@@ -41,133 +42,133 @@ export class QuizListComponent {
 
 
   // Sample data to use for assessment (to be removed)
-  quizes: Quiz[] = [
-    {
-      quizCount: 5,
-      assignments: [
-        {
-          id: 1,
-          title: "Quiz 1: Introduction to JavaScript",
-          dateCreated: "2024-01-15",
-          type: "Quiz",
-          action: "Take Now",
-          questionCount: 2,
-        },
-        {
-          id: 2,
-          title: "Quiz 2: HTML Basics",
-          dateCreated: "2024-01-18",
-          type: "Lab",
-          action: "Take Now",
-          questionCount: 2,
-        },
-        {
-          id: 3,
-          title: "Quiz 3: CSS Styling",
-          dateCreated: "2024-01-20",
-          type: "Presentation",
-          action: "Take Now",
-          questionCount: 1,
-        },
-        {
-          id: 4,
-          title: "Quiz 4: Advanced JavaScript",
-          dateCreated: "2024-01-25",
-          type: "Quiz",
-          action: "Take Now",
-          questionCount: 2,
-        },
-        {
-          id: 5,
-          title: "Quiz 5: Angular Fundamentals",
-          dateCreated: "2024-01-30",
-          type: "Lab",
-          action: "Take Now",
-          questionCount: 2,
-        },
-        {
-          "id": 6,
-          "title": "Quiz 6: TypeScript Basics",
-          "dateCreated": "2024-02-05",
-          "type": "Quiz",
-          "action": "Take Now",
-          "questionCount": 3
-        },
-        {
-          "id": 7,
-          "title": "Quiz 7: React Introduction",
-          "dateCreated": "2024-02-10",
-          "type": "Lab",
-          "action": "Take Now",
-          "questionCount": 4
-        },
-        {
-          "id": 8,
-          "title": "Quiz 8: Web Development Best Practices",
-          "dateCreated": "2024-02-15",
-          "type": "Presentation",
-          "action": "Take Now",
-          "questionCount": 2
-        },
-        {
-          "id": 9,
-          "title": "Quiz 9: Frontend Frameworks",
-          "dateCreated": "2024-02-18",
-          "type": "Quiz",
-          "action": "Take Now",
-          "questionCount": 5
-        },
-        {
-          "id": 10,
-          "title": "Quiz 10: Backend Development with Node.js",
-          "dateCreated": "2024-02-22",
-          "type": "Lab",
-          "action": "Take Now",
-          "questionCount": 3
-        },
-        {
-          "id": 11,
-          "title": "Quiz 11: Database Design Fundamentals",
-          "dateCreated": "2024-02-25",
-          "type": "Presentation",
-          "action": "Take Now",
-          "questionCount": 4
-        },
-        {
-          "id": 12,
-          "title": "Quiz 12: DevOps Essentials",
-          "dateCreated": "2024-03-01",
-          "type": "Quiz",
-          "action": "Take Now",
-          "questionCount": 3
-        },
-        {
-          "id": 13,
-          "title": "Quiz 13: Cloud Computing Overview",
-          "dateCreated": "2024-03-05",
-          "type": "Lab",
-          "action": "Take Now",
-          "questionCount": 2
-        },
-        {
-          "id": 14,
-          "title": "Quiz 14: Advanced CSS Techniques",
-          "dateCreated": "2024-03-08",
-          "type": "Presentation",
-          "action": "Take Now",
-          "questionCount": 1
-        },
-        {
-          "id": 15,
-          "title": "Quiz 15: Software Testing and Quality Assurance",
-          "dateCreated": "2024-03-12",
-          "type": "Quiz",
-          "action": "Take Now",
-          "questionCount": 3
-        }
-      ]
-    }
-  ];
+  // quizes: Quiz[] = [
+  //   {
+  //     quizCount: 5,
+  //     assignments: [
+  //       {
+  //         id: 1,
+  //         title: "Quiz 1: Introduction to JavaScript",
+  //         dateCreated: "2024-01-15",
+  //         type: "Quiz",
+  //         action: "Take Now",
+  //         questionCount: 2,
+  //       },
+  //       {
+  //         id: 2,
+  //         title: "Quiz 2: HTML Basics",
+  //         dateCreated: "2024-01-18",
+  //         type: "Lab",
+  //         action: "Take Now",
+  //         questionCount: 2,
+  //       },
+  //       {
+  //         id: 3,
+  //         title: "Quiz 3: CSS Styling",
+  //         dateCreated: "2024-01-20",
+  //         type: "Presentation",
+  //         action: "Take Now",
+  //         questionCount: 1,
+  //       },
+  //       {
+  //         id: 4,
+  //         title: "Quiz 4: Advanced JavaScript",
+  //         dateCreated: "2024-01-25",
+  //         type: "Quiz",
+  //         action: "Take Now",
+  //         questionCount: 2,
+  //       },
+  //       {
+  //         id: 5,
+  //         title: "Quiz 5: Angular Fundamentals",
+  //         dateCreated: "2024-01-30",
+  //         type: "Lab",
+  //         action: "Take Now",
+  //         questionCount: 2,
+  //       },
+  //       {
+  //         "id": 6,
+  //         "title": "Quiz 6: TypeScript Basics",
+  //         "dateCreated": "2024-02-05",
+  //         "type": "Quiz",
+  //         "action": "Take Now",
+  //         "questionCount": 3
+  //       },
+  //       {
+  //         "id": 7,
+  //         "title": "Quiz 7: React Introduction",
+  //         "dateCreated": "2024-02-10",
+  //         "type": "Lab",
+  //         "action": "Take Now",
+  //         "questionCount": 4
+  //       },
+  //       {
+  //         "id": 8,
+  //         "title": "Quiz 8: Web Development Best Practices",
+  //         "dateCreated": "2024-02-15",
+  //         "type": "Presentation",
+  //         "action": "Take Now",
+  //         "questionCount": 2
+  //       },
+  //       {
+  //         "id": 9,
+  //         "title": "Quiz 9: Frontend Frameworks",
+  //         "dateCreated": "2024-02-18",
+  //         "type": "Quiz",
+  //         "action": "Take Now",
+  //         "questionCount": 5
+  //       },
+  //       {
+  //         "id": 10,
+  //         "title": "Quiz 10: Backend Development with Node.js",
+  //         "dateCreated": "2024-02-22",
+  //         "type": "Lab",
+  //         "action": "Take Now",
+  //         "questionCount": 3
+  //       },
+  //       {
+  //         "id": 11,
+  //         "title": "Quiz 11: Database Design Fundamentals",
+  //         "dateCreated": "2024-02-25",
+  //         "type": "Presentation",
+  //         "action": "Take Now",
+  //         "questionCount": 4
+  //       },
+  //       {
+  //         "id": 12,
+  //         "title": "Quiz 12: DevOps Essentials",
+  //         "dateCreated": "2024-03-01",
+  //         "type": "Quiz",
+  //         "action": "Take Now",
+  //         "questionCount": 3
+  //       },
+  //       {
+  //         "id": 13,
+  //         "title": "Quiz 13: Cloud Computing Overview",
+  //         "dateCreated": "2024-03-05",
+  //         "type": "Lab",
+  //         "action": "Take Now",
+  //         "questionCount": 2
+  //       },
+  //       {
+  //         "id": 14,
+  //         "title": "Quiz 14: Advanced CSS Techniques",
+  //         "dateCreated": "2024-03-08",
+  //         "type": "Presentation",
+  //         "action": "Take Now",
+  //         "questionCount": 1
+  //       },
+  //       {
+  //         "id": 15,
+  //         "title": "Quiz 15: Software Testing and Quality Assurance",
+  //         "dateCreated": "2024-03-12",
+  //         "type": "Quiz",
+  //         "action": "Take Now",
+  //         "questionCount": 3
+  //       }
+  //     ]
+  //   }
+  // ];
   
   ngOnInit() {
     this.init();
@@ -179,26 +180,7 @@ export class QuizListComponent {
     this.traineeEmail = this.decodedToken?.email;
 
     this.assessments$ = this.traineeAssessentsService.getAllAssignments(this.traineeEmail)
-
-    this.assessments$ = of(this.quizes).pipe(
-      map((quizes) => {
-        // Filter assignments based on assessmentType
-        const filteredAssignments = quizes[0].assignments.filter(
-          (assignment) =>
-            this.assessmentType === '' ||
-            assignment.type.toLowerCase() === this.assessmentType.toLowerCase()
-        );
-
-        // Set totallyEmpty to true if no assignments match
-        this.totallyEmpty = filteredAssignments.length === 0;
-
-        // Emit the result to the parent
-        this.dataEmpty.emit(this.totallyEmpty);
-
-        // Return the filtered assignments
-        return filteredAssignments;
-      })
-    );
+    this.assessments$.subscribe(data => console.log(data))
 
 
     this.filteredAssessment$ = combineLatest([
@@ -207,9 +189,12 @@ export class QuizListComponent {
     ]).pipe(
       map(([assessments, searchTerm]) => {
         // Filter assignments by search term
-        return assessments.filter(assignment =>
-          assignment.title.toLowerCase().includes(searchTerm.toLowerCase())
-        );
+        const title = assessments.filter((quiz:any) => quiz.assessment.title)
+        console.log("title: ", title)
+        return title;
+        // return assessments.filter(assignment =>
+        //   assignment.title.toLowerCase().includes(searchTerm.toLowerCase())
+        // );
       })
     );
 
