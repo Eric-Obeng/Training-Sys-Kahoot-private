ba7f525f36ac8ad4a77f4d6ac50b360838176cf5
Silas Owu <owusilasyp00@gmail.com>
2024-12-16 12:28:34 +0000

    chore: update environment variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  BaseUrl: 'https://api.training-management.amalitech-dev.net/api/v1',
+  BaseUrl: 'https://octopus-arriving-monthly.ngrok-free.app/api/v1',
 };
