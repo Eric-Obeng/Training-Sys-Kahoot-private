2fe4e38cb1164d47240bca78d9af4b73d143070e
Silas Owu <owusilasyp00@gmail.com>
2024-12-17 08:52:00 +0000

    feat: improve error handling service with notification display

--- src/app/features/admin/specializations/create/create-specialization.component.ts
+++ src/app/features/admin/specializations/create/create-specialization.component.ts
@@ -1,19 +1,21 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from "../form/form.component";
 import { SpecializationFacadeService } from '@core/services/specialization-facade/specialization-facade.service';
 import { specialization } from '@core/models/specialization.interface';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { of,timer } from 'rxjs';
 import { AddFeedbackComponent } from "../add-feedback/add-feedback.component";
+import { ErrorHandleService } from '@core/services/error-handle/error-handle.service';
 
 
 @Component({
   selector: 'app-create-specialization',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, FormComponent, RouterLink, AddFeedbackComponent],
+  imports: [ReactiveFormsModule, CommonModule, FormComponent, AddFeedbackComponent,],
   templateUrl: './create-specialization.component.html',
   styleUrl: './create-specialization.component.scss'
 })
@@ -26,7 +28,8 @@ export class CreateSpecializationComponent {
 
   constructor(private router: Router,
     private facadeService: SpecializationFacadeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private errorService: ErrorHandleService
   ){}
 
   ngOnInit(){
@@ -44,35 +47,38 @@ export class CreateSpecializationComponent {
         this.specializationData = specialization;
         this.isLoading = false;
       },
-      error: (error) => {
-        console.error('Error fetching specialization:', error);
+      error: (error: HttpErrorResponse) => {
         this.isLoading = false;
+        this.errorService.showErrorSnackbar('Error fetching specialization:'+ error.message);
       }
     });
   }
 
   handleFormSubmit(formData: specialization) {
     const formOperation = this.specializationData?.id ?
-      this.facadeService.update(this.specializationData.id, formData)
-      : this.facadeService.create(formData);
+    this.facadeService.update(this.specializationData.id, formData)
+    : this.facadeService.create(formData);
     formOperation.subscribe({
       next: () => {
         this.showFeedback = true;
-        timer(3000).subscribe(()=>{
+        const successMessage = this.specializationData?.id ?
+          'Specialization updated successfully!' :
+          'Specialization created successfully!';
+        this.errorService.showSuccessSnackbar(successMessage);
+
+        timer(3000).subscribe(() => {
           this.showFeedback = false;
-          this.navigateToList()
-        })
+          this.navigateToList();
+        });
       },
-      error: (error) => {
-        this.handleError(error)
+      error: (error: HttpErrorResponse) => {
+        const operation = this.specializationId ? 'updating' : 'creating';
+        const errorMessage = error.message || `Error ${operation} specialization. Please try again.`;
+        this.errorService.showErrorSnackbar(errorMessage);
       }
-    })
+    });
   }
 
-   private handleError(error: any): void {
-    const operation = this.specializationId ? 'updating' : 'creating';
-    throw new Error(`Error ${operation} specialization: ${error}`);
-  }
 
   navigateToList(){
     this.router.navigate(['home','admin','specialization-management'])
