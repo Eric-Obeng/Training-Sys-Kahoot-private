24b745bccfe1c99b4467fb9df64aba57ea57a0ad
Kojoel <nanaabruquah1s@gmail.com>
2024-12-20 07:31:06 +0000

    feat: fix header titles

--- src/app/core/services/active-nav/active-nav.service.ts
+++ src/app/core/services/active-nav/active-nav.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class ActiveNavService {
   private currentNav = new BehaviorSubject<string>('Dashboard');
   currentNavSubject$ = this.currentNav.asObservable();
 
-  constructor() {}
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateNavFromUrl(event.urlAfterRedirects);
+      }
+    });
+  }
 
   setcurrentNav(nav: string) {
     this.currentNav.next(nav);
+  }
+
+  private updateNavFromUrl(url: string) {
+    const match = url.match(/(?:admin|trainer|trainee)\/([^\/]+)/);
+    let extractedNav = match ? match[1] : 'Dashboard';
+    extractedNav = extractedNav
+      .replace(/-/g, ' ')
+      .replace(/\b\w/g, char => char.toUpperCase());
+    this.setcurrentNav(extractedNav);
   }
 }
